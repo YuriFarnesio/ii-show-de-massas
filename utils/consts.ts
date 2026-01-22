59d8f6d7f52b677ccd6d0a379a30d85a885c2df4
Yuri Farnesio <yuri.farnesio@gmail.com>
2026-01-21 23:00:51 -0300

    test: change value to test

--- utils/consts.ts
+++ utils/consts.ts
@@ -4,13 +4,13 @@ export type TicketType = (typeof TICKET_TYPES)[number];
 
 export const PRICES: Record<TicketType, number> = {
   inteira: 5000,
-  juvenil: 3500,
+  juvenil: 150,
   infantil: 0,
 };
 
 export const TICKET_LABELS: Record<TicketType, string> = {
-  inteira: "Adulto (acima de 16 anos) - R$ 50",
-  juvenil: "Jovem (12 a 16 anos) - R$ 35",
+  inteira: "Adulto (acima de 16 anos) - R$ 50,00",
+  juvenil: "Jovem (12 a 16 anos) - R$ 1,50",
   infantil: "Criança - Gratuito",
 };
 
